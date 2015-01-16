75bb6c1e09d0ad94779d948e20267370d30ab049
Sroop Sunar <sroopsunar@hotmail.com>
2015-01-16 12:59:52 +0000

    World size is set on initialization and can populate with cells at their proper coordinates.

--- conways-game-of-life/lib/world.rb
+++ conways-game-of-life/lib/world.rb
@@ -1,11 +1,21 @@
 class World
 
-  attr_accessor :cells, :alive_cells, :dead_cells
+  attr_accessor :cells, :alive_cells, :dead_cells, :rows, :cols
 
-  def initialize
+  def initialize(rows=3,cols=3)
     @cells = []
     @alive_cells = []
     @dead_cells = []
+    @rows,@cols = rows,cols
+  end
+
+  def populate
+    master_cell = cells.first
+    (master_cell.x...rows).each do |x|
+      (master_cell.x...cols).each do |y|
+        master_cell.spawn_at_coordinate(x,y) unless [x,y] == [master_cell.x, master_cell.y]
+      end
+    end
   end
 
   def tick!
--- conways-game-of-life/spec/world_spec.rb
+++ conways-game-of-life/spec/world_spec.rb
@@ -8,6 +8,20 @@ describe "World" do
 
   context "Utility methods" do
 
+    it "initializes by setting the size of the world" do
+      expect(world.rows).to eq(3)
+      expect(world.cols).to eq(3)
+      new_world = World.new(6,6)
+      expect(new_world.rows).to eq(6)
+      expect(new_world.cols).to eq(6)
+    end
+
+    it "populates cells" do
+      expect(world.cells).to eq([subject])
+      world.populate
+      expect(world.cells.size).to eq(9)
+    end
+
     it "detects all alive cells" do
       subject.spawn_at_coordinate(1,0)
       subject.spawn_at_coordinate(1,1)
