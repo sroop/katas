166510d30c43e2603f3bbe7b7073da2ef8e61a20
Sroop Sunar <sroopsunar@hotmail.com>
2015-01-28 16:25:29 +0000

    Set game board background colour.

--- conways-game-of-life/lib/game_of_life_board.rb
+++ conways-game-of-life/lib/game_of_life_board.rb
@@ -4,9 +4,16 @@ require_relative 'world.rb'
 
 class GameOfLifeWindow < Gosu::Window
 
-  def initialize
-    super 640, 480, false
+  def initialize(width=640, height=480 )
+    super @width=width, @height=height, false
     self.caption = "Game Of Life"
+    @bg_colour = Gosu::Color.new(0xffdedede)
+  end
+  def draw
+    draw_quad(0,0,@bg_colour,
+              @width,0,@bg_colour,
+              @width,@height,@bg_colour,
+              0,@height,@bg_colour)
   end
 
 end
