3fc9ba7a176db0b725403ffce612f3045c9db99e
Sroop Sunar <sroopsunar@hotmail.com>
2015-01-28 17:42:23 +0000

    Apply colour to all live cells to make them visible on the game board.

--- conways-game-of-life/lib/game_of_life_board.rb
+++ conways-game-of-life/lib/game_of_life_board.rb
@@ -5,13 +5,17 @@ require_relative 'world.rb'
 class GameOfLifeWindow < Gosu::Window
 
   attr_accessor :width, :height
+  attr_reader :rows, :cols, :world
 
   BACKGROUND_COLOUR = Gosu::Color.new(0xffdedede)
+  LIVE_CELL_COLOUR = Gosu::Color.new(0xffff69b4)
+  CELL_WIDTH, CELL_HEIGHT = 10, 10
 
   def initialize(width=640, height=480 )
     @width, @height = width, height
     super width, height, false
     self.caption = "Game Of Life"
+    @rows, @cols = width/CELL_WIDTH, height/CELL_HEIGHT
     create_world
   end
 
@@ -31,6 +35,19 @@ class GameOfLifeWindow < Gosu::Window
               width, 0, BACKGROUND_COLOUR,
               width, height, BACKGROUND_COLOUR,
               0, height, BACKGROUND_COLOUR)
+
+    world.alive_cells.each do |cell|
+      draw_quad(
+                # Bottom left cell corner
+                cell.x * CELL_WIDTH, cell.y * CELL_HEIGHT, LIVE_CELL_COLOUR,
+                # Bottom right cell corner
+                cell.x * CELL_WIDTH + CELL_WIDTH, cell.y * CELL_HEIGHT, LIVE_CELL_COLOUR,
+                # Top right cell corner
+                cell.x * CELL_WIDTH + CELL_WIDTH, cell.y * CELL_HEIGHT + CELL_HEIGHT, LIVE_CELL_COLOUR,
+                # Top left cell corner
+                cell.x * CELL_WIDTH, cell.y * CELL_HEIGHT + CELL_HEIGHT, LIVE_CELL_COLOUR,
+                )
+    end
   end
 
 end
