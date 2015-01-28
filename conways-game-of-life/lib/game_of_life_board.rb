c5d6f26ccaf1aa3eb0d178bf29720bb5a6b86a8b
Sroop Sunar <sroopsunar@hotmail.com>
2015-01-28 16:26:51 +0000

    Initialise game board with a seeded and populated world of cells.

--- conways-game-of-life/lib/game_of_life_board.rb
+++ conways-game-of-life/lib/game_of_life_board.rb
@@ -8,7 +8,13 @@ class GameOfLifeWindow < Gosu::Window
     super @width=width, @height=height, false
     self.caption = "Game Of Life"
     @bg_colour = Gosu::Color.new(0xffdedede)
+
+    @world = World.new(@width/10, @height/10)
+    @cell = Cell.new(@world)
+    @world.seed
+    @world.populate
   end
+
   def draw
     draw_quad(0,0,@bg_colour,
               @width,0,@bg_colour,
